d1139c814ecc0e21302fcb41909f015fe3c70a3b
bluetata <dietime1943@gmail.com>
2017-06-30 19:33:43 +0800

    section01 parts of 02 (input file) is added
    Signed-off-by: bluetata <dietime1943@gmail.com>

--- src/manual/cookbook/org/guideline/sections01/JsoupInputAndOutput.java
+++ src/manual/cookbook/org/guideline/sections01/JsoupInputAndOutput.java
@@ -1,9 +1,12 @@
 package org.guideline.sections01;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Session01:Jsoup 数据输入与输出
@@ -12,7 +15,13 @@ public class JsoupInputAndOutput {
 
     public static void main(String[] args) {
 
-       jsoupIOTest01();
+       // jsoupIOTest01();
+        try {
+            jsoupIOTest02();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
     
@@ -26,8 +35,34 @@ public class JsoupInputAndOutput {
                              "     <h1>Hello World!</h1>" +
                           // "   </body>" +
                              " </html>";
-        Document doc1 = Jsoup.parse(htmlText);
-        System.out.println(doc1.html());
+        Document doc = Jsoup.parse(htmlText);
+        System.out.println(doc.html());
     }
+    
+    
+    public static void jsoupIOTest02() throws IOException{
+
+        String fileName = "../jsoup-manual-cookbook/src/manual/resources/section01.html";
+        File in = new File(fileName);
+        Document doc = Jsoup.parse(in, "UTF-8", "https://github.com/bluetata/");
+
+        System.out.println(doc.select("img").first().absUrl("src"));
+        System.out.println(doc.select("a[href]").first().absUrl("href"));
+        
+        System.out.println("====================================");
+        
+        System.out.println(doc.html());
+    }
+    
+    public static void jsoupIOTest03() throws IOException{
+
+        // https://github.com/jhy/jsoup/issues/440
+        Connection.Response res = Jsoup.connect("http://direct.infohound.net/tools/charset-base.html").execute();
+        Document doc = res.parse();
+        // assertEquals("http://example.com/foo.jpg", doc.select("img").first().absUrl("src"));
+        
+        System.out.println(doc.select("img").first().absUrl("src"));
+        
+       }
 
 }
