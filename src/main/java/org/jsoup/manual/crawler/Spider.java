fe814fc9beac5c0734d47517c2d3f374404cb451
bluetata <Sekito.Lv@gmail.com>
2019-03-20 17:56:03 +0800

    Update spider

--- src/main/java/org/jsoup/manual/crawler/Spider.java
+++ src/main/java/org/jsoup/manual/crawler/Spider.java
@@ -21,18 +21,53 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * @date 03/20/19 10:28</br>
- * @version jsoup-manual version(1.0)</br>
  * @author bluetata / Sekito.Lv@gmail.com</br>
+ * @version jsoup-manual version(1.0)</br>
+ * @date 03/20/19 10:28</br>
  * @since JDK 1.8</br>
  */
 public class Spider {
-    // Fields
     private static final int MAX_PAGES_TO_SEARCH = 10;
     private Set<String> pagesVisited = new HashSet<String>();
     private List<String> pagesToVisit = new LinkedList<String>();
 
 
+    /**
+     * Our main launching point for the Spider's functionality. Internally it creates spider legs
+     * that make an HTTP request and parse the response (the web page).
+     *
+     * @param url        - The starting point of the spider
+     * @param searchWord - The word or string that you are searching for
+     */
+    public void search(String url, String searchWord) {
+        while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
+            String currentUrl;
+            SpiderLeg leg = new SpiderLeg();
+            if (this.pagesToVisit.isEmpty()) {
+                currentUrl = url;
+                this.pagesVisited.add(url);
+            } else {
+                currentUrl = this.nextUrl();
+            }
+            leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
+            // SpiderLeg
+            boolean success = leg.searchForWord(searchWord);
+            if (success) {
+                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
+                break;
+            }
+            this.pagesToVisit.addAll(leg.getLinks());
+        }
+        System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
+    }
+
+
+    /**
+     * Returns the next URL to visit (in the order that they were found). We also do a check to make
+     * sure this method doesn't return a URL that has already been visited.
+     *
+     * @return
+     */
     private String nextUrl() {
         String nextUrl;
         do {
